2aaa723936cb5e0d634702401ebfc8fe2921f710
Rahman Ecky <rahmanecky.r@gmail.com>
2022-04-08 18:40:42 +0700

    Initial commit

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,38 @@
+object ApplicationId{
+    /*
+    * pusdokes = com.cexup.app.pusdokkes
+    * ams = com.cexup.app.ams
+    * cexup = com.cexup.app
+    * rsui = com.cexup.app.rsui
+    * imunn = com.cexup.app.imunn
+    * */
+    const val idConsumer = "com.cexup.app.pusdokkes"
+
+    /*
+    * pusdokes = com.cexup.corp.pusdokkes
+    * ams = com.cexup.corp.ams
+    * cexup = com.cexup.corp
+    * rsui = com.cexup.corp.rsui
+    * imunn = com.cexup.corp.imunn
+    * */
+    const val idCorporate = "com.cexup.corp.pusdokkes"
+    /*
+    * pusdokes = com.cexup.doctor.pusdokkes
+    * ams = com.cexup.doctor.ams
+    * cexup = com.cexup.doctor
+    * rsui = com.cexup.doctor.rsui
+    * imunn = com.cexup.doctor.imunn
+    * */
+    const val idDoctor = "com.cexup.doctor.pusdokkes"
+}
+
+
+
+
+
+
+/**
+ * Arranged alphabetically
+ */
+
+
